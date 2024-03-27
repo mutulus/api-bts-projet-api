9ec7144c6cc0535ad2a10592a310ad82c91ae4dd
Arturs Mednis <artursmednis2003@gmail.com>
2024-03-27 08:06:34 +0100

    fin lister films à l'affiche

--- src/Repository/FilmRepository.php
+++ src/Repository/FilmRepository.php
@@ -41,58 +41,14 @@ class FilmRepository extends ServiceEntityRepository
     public function findFilmAffiche(): array
     {
         $date = new \DateTime();
-          return $this->createQueryBuilder('f')
-              ->select('f')
-              ->from(Seance::class, 's')
-              ->join('s.film', 'fs')
-              ->where('s.dateProjection >= :date')
-              ->setParameter('date', $date)
-              ->orderBy('s.dateProjection', 'ASC')
-              ->getQuery()
-              ->getResult();
-//
-//->getEntityManager()
-//            ->createQuery(
-//                'SELECT f
-//            FROM App\Entity\Seance as s
-//            JOIN s.film as f
-//            WHERE s.dateProjection >= CURRENT_TIMESTAMP()'
-//            )
-//            ->getResult();
-
-//        return $this->createQueryBuilder('f')
-//            ->select('f')
-//            ->from('App:Seance' ,'s')
-//            ->innerJoin('f.id', 's')
-//            ->where('s.dateProjection >= :date')
-//            ->setParameter('date', $date)
-//            ->orderBy('s.dateProjection', 'ASC')
-//            ->getQuery()
-//            ->getResult();
+        return $this->createQueryBuilder('f')
+            ->select('f')
+            ->innerJoin('f.seances', 's')
+            ->where('s.dateProjection >= :date')
+            ->setParameter('date', $date)
+            ->orderBy('s.dateProjection', 'ASC')
+            ->getQuery()
+            ->getResult();
     }
 
-    //    /**
-    //     * @return Film[] Returns an array of Film objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('f')
-    //            ->andWhere('f.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('f.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Film
-    //    {
-    //        return $this->createQueryBuilder('f')
-    //            ->andWhere('f.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
